39157936662ab1736718da5d425c526e2552667d
RossGit97 <grsucalit@gmail.com>
2026-01-08 22:24:26 +0800

    Added Player class

---
+++ Cards Project/Cards Project/Models/Player.cs
@@ -0,0 +1,31 @@
+﻿using CardsProject.Models;
+using System;
+
+public class Player
+{
+    public String Name { get; }
+
+    private List<Card> Hand;
+
+    public int HandSize => Hand.Count;
+
+    public Player(String name)
+    {
+        Name = name; 
+        Hand = new List<Card>();
+    }
+
+    public void DrawFromDeck(Deck deck)
+    {
+        Hand.Add(deck.DrawCard());
+    }
+
+    public void ShowHand()
+    {
+        for (int x = 0; x < HandSize; x++)
+        {
+            Console.WriteLine(Hand[x].ToString());
+        }
+    }
+}
+
--- Cards Project/Cards Project/Program.cs
+++ Cards Project/Cards Project/Program.cs
@@ -8,11 +8,16 @@ class Program
         Console.WriteLine("Card");
         Deck new_deck = new Deck();
         new_deck.Shuffle();
-        while (new_deck.DeckSize > 0)
-        {
-            Console.WriteLine(new_deck.DrawCard().ToString());
-            Console.WriteLine(new_deck.DeckSize);
-        }
-        
+        string name = Console.ReadLine();
+        Player p1 = new Player(name);
+        //while (new_deck.DeckSize > 0)
+        //{
+        //    Console.WriteLine(new_deck.DrawCard().ToString());
+        //    Console.WriteLine(new_deck.DeckSize);
+        //}
+        Console.WriteLine($"Player name is {p1.Name}");
+        p1.DrawFromDeck( new_deck );
+        Console.WriteLine($"{p1.Name} has {p1.HandSize} cards");
+        p1.ShowHand();
     }
 }
